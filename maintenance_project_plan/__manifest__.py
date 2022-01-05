ea7480fb1d2990e0d2ff0ea25adf6eab649bd679
Zach Waite <zach@waiteperspectives.com>
2022-01-05 11:54:53 -0500

    [MIG][15.0] maintenance_project_plan:

--- maintenance_project_plan/__manifest__.py
+++ maintenance_project_plan/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Maintenance Project Plans",
     "summary": "Adds project and task to a Maintenance Plan",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Odoo Community Association (OCA), Solvos",
     "license": "AGPL-3",
     "category": "Maintenance",
